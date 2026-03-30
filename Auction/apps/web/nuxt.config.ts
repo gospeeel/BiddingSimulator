ff0e4effe7b7f02761543cb4296a7790c21ebf9d
Danill <danila.ostroverhov@yandex.ru>
2026-03-30 14:43:20 +0300

    chore(repo): configure turborepo (fastify + nuxt)

---
+++ Auction/apps/web/nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  compatibilityDate: "2026-03-30",
+  modules: ["@nuxt/eslint"],
+});
